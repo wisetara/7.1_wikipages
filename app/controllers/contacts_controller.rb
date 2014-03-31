a0bfdfa0839822e1780b110c7974431749aaf961
Jason Mannon & Tara Scherner de la Fuente <student@epicodus.com>
2014-03-31 11:38:47 -0700

    finished wikipages exercise

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -15,6 +15,17 @@ class ContactsController < ApplicationController
     render('contacts/show.html.erb')
   end
 
+  def edit
+    @contact = Contact.find(params[:id])
+    render('contacts/edit.html.erb')
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render('contacts/destroy.html.erb')
+  end
+
   def create
     @contact = Contact.create(:name => params[:name],
                               :email => params[:email],
@@ -23,6 +34,17 @@ class ContactsController < ApplicationController
       render('contacts/success.html.erb')
     else
       render('contacts/new.html.erb')
+    end
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(:name => params[:name],
+                       :email => params[:email],
+                       :phone => params[:phone])
+      render('contacts/success.html.erb')
+    else
+      render('contacts/edit.html.erb')
     end
   end
 end
